62915d8db0ede57f8daa00ac74ec644a17a17530
kimmi15 <96224700+kimmi15@users.noreply.github.com>
2022-03-20 22:20:15 +0530

    Add files via upload

---
+++ build/precache-manifest.bc2d3f77ab489f834de4b6b18207ccc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ee66cc3a6e01add6c34",
+    "url": "/static/css/main.c8b033b3.chunk.css"
+  },
+  {
+    "revision": "1ee66cc3a6e01add6c34",
+    "url": "/static/js/main.1ee66cc3.chunk.js"
+  },
+  {
+    "revision": "98fae294dabfb7543782",
+    "url": "/static/js/1.98fae294.chunk.js"
+  },
+  {
+    "revision": "8d22b33b586c518edd90",
+    "url": "/static/css/2.89adfb2d.chunk.css"
+  },
+  {
+    "revision": "8d22b33b586c518edd90",
+    "url": "/static/js/2.8d22b33b.chunk.js"
+  },
+  {
+    "revision": "b600c0325db6bc8bf28f",
+    "url": "/static/js/runtime~main.b600c032.js"
+  },
+  {
+    "revision": "e0ec3b5082cd81b6ff7ed93f2e7d1871",
+    "url": "/index.html"
+  }
+];
